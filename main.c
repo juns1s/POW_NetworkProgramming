46f3058141e788567fe57516b805fd85140ae870
clap_0 <clap-0@naver.com>
2023-06-19 00:39:23 +0900

    feat: 작업서버 기능 추가

--- main.c
+++ main.c
@@ -7,22 +7,22 @@
 int main(){
     char *challenge = "201928732019283320192836";
     char sha256Hash[65];
-    char nonce[20];
+    unsigned int nonce;
 
     int difficulty;
-    int startNonce = 0;
-    int nonceRange = 1000000000;
+    unsigned int startNonce = 0;
+    unsigned int nonceRange = 1000000000;
 
     printf("difficulty: ");
     scanf("%d", &difficulty);
 
     clock_t start = clock();
 
-    findNonce(nonce, sha256Hash, challenge, difficulty,startNonce,nonceRange);
+    findNonce(&nonce, sha256Hash, challenge, difficulty,startNonce,nonceRange);
 
     clock_t end = clock();
 
-    printf("Challenge + Nonce: %s + %s\n", challenge, nonce);
+    printf("Challenge + Nonce: %s + %u\n", challenge, nonce);
     printf("Hash: %s\n", sha256Hash);
     printf("Total Time: %lf\n", (double)(end - start)/ CLOCKS_PER_SEC);
 
